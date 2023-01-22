9440d9e97813238aff69a10c200e5f8d2db1ee3d
Shriyash <appollo3579@gmail.com>
2023-01-22 20:09:23 +0530

    Added previous events page

---
+++ Assests/JS/previous-index.js
@@ -0,0 +1,21 @@
+
+
+  
+
+//  $('.thumb-wrapper').on('click',function(){
+//   swal("Hello world!");
+//  });
+
+//  var name = "John Doe";
+// var bio = "Software developer with 5 years of experience";
+// var imgUrl = "https://example.com/images/johndoe.jpg";
+
+// document.getElementsByClassName("thumb-wrapper")[0].addEventListener("click", function(){
+//   var message = "<img src='" + imgUrl + "' alt='" + name + "'>";
+//   message += "<br>Name: " + name + "<br>Bio: " + bio;
+
+//   document.getElementById("messageBox").innerHTML = message;
+//   alert(document.getElementById("messageBox").innerHTML);
+// });
+
+
