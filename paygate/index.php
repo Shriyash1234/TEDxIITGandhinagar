46c77291088417406041cfa7ce037e1769063236
Shriyash <appollo3579@gmail.com>
2023-02-11 17:44:01 +0530

    Integrated the payment and improved the interface of payment

--- paygate/index.php
+++ paygate/index.php
@@ -7,37 +7,12 @@
 <?php include '../Assets/CSS/sample.css'; ?>
 </style>  
 
-<!-- Include Nav-Bar -->
-<div class="nav nav-bar">
-    <input type="checkbox" id="nav-check">
-    <div class="nav-header">
-      <img class="tedx-image" src="Assests/img/tedx_IITB.png" alt="">
-    </div>
-    <div class="nav-btn">
-      <label for="nav-check">
-        <span></span>
-        <span></span>
-        <span></span>
-      </label>
-    </div>
-    
-    <div class="nav-links">
-      <a class="section-item" href="#" >Home</a>
-      <a class="section-item" href="#About-tedx" >About</a>
-      <a class="section-item" href="#speakers" >Speakers</a>
-      <a class="section-item" href="#" >Talk</a>
-      <a class="section-item" href="team.html">Team</a>
-      <a href="registeration.html"><button class="get-tickets">Get Tickets</button></a>
-    </div>
-    
-</div>
-
 <!-- HTML | Forms -->
 <div class="mainscreen">
   <div class="card">
     <div class="leftside">
       <img
-           src="https://i.pinimg.com/originals/18/9d/dc/189ddc1221d9c1c779dda4ad37a35fa1.png"
+           src="../Assests/img/TEDxIITGandhinagar_Logo.jpg"
            class="product"
            alt="Shoes"
            />
@@ -59,44 +34,6 @@
   </div>
 </div>
 
-<!-- Footer Section -->
-<section class="footer-class">
-  <footer>
-    <div class="container">
-      <div class="row">
-        <div class="footer-col col-sm-3">
-          <h4>About TEDx</h4>
-          <p>TEDx is a local, self-organized event that brings people together to share a TED-like experience.</p>
-        </div>
-        <div class="footer-col col-sm-3">
-          <h4>Explore</h4>
-          <ul>
-            <li><a href="#">TEDx Talks</a></li>
-            <li><a href="#">Organize a TEDx Event</a></li>
-            <li><a href="#">Become a TEDx Partner</a></li>
-          </ul>
-        </div>
-        <div class="footer-col col-sm-3">
-          <h4>Follow Us</h4>
-          <ul>
-            <li><a href="#">Facebook</a></li>
-            <li><a href="#">Twitter</a></li>
-            <li><a href="#">Instagram</a></li>
-          </ul>
-        </div>
-        <div class="footer-col col-sm-3">
-          <h4>Contact</h4>
-          <ul>
-            <li>TEDx</li>
-            <li>123 Main Street</li>
-            <li>Anytown, USA</li>
-            <li>123-456-7890</li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </footer>
-</section>
 
 <!-- Activates Razorpay pop-ups onclicking the check-out button -->
 <script>
@@ -114,7 +51,7 @@
                     "currency": "INR",
                     "name": "TEDxIITGandhinagar",
                     "description": "Test Transaction",
-                    "image": "https://avatars.githubusercontent.com/u/33686558?s=280&v=4",
+                    // "image": "Assests/img/TEDxIITGandhinagar_Logo.jpg",
                     "handler": function (response){
                         // console.log(response);
                         
